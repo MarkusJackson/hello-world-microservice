8f89a1ae0fb26f3b65597355c6b785901a4bcdcd
MarkusJackson <markusactionjackson@gmail.com>
2025-05-01 13:03:36 +0200

    Initial commit

---
+++ src/main/java/de/minikube/HelloWorldController.java
@@ -0,0 +1,13 @@
+package de.minikube;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello, World!";
+    }
+}
